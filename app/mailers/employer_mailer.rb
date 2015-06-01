5a5e6cf9984e00b2f6523c10592ed9f8ff17d476
petedigipulse <peter@digitalpulse.co.nz>
2015-06-02 11:47:04 +1200

    Added send mail to employer in controller and added HTML to email template with link to account

--- app/mailers/employer_mailer.rb
+++ app/mailers/employer_mailer.rb
@@ -1,7 +1,8 @@
 class EmployerMailer < ApplicationMailer
-
+	default from: 'peter@digitalpulse.co.nz'
 	def welcome_email(employer)
 		@employer = employer
+		@url = 
 		mail(to: @employer.email, subject: 'test welcome email')
 	end
 end
