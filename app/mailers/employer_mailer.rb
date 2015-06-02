a31cec51311b6ac5793350f27a437cb0d82078d4
petedigipulse <peter@digitalpulse.co.nz>
2015-06-02 13:29:23 +1200

    Added Candidate Dashboard with styling

--- app/mailers/employer_mailer.rb
+++ app/mailers/employer_mailer.rb
@@ -1,7 +1,7 @@
 class EmployerMailer < ApplicationMailer
 	default from: 'peter@digitalpulse.co.nz'
 	def welcome_email(employer_params)
-		@employer = employer_params(params[:id], params[:first_name])
+		@employer = employer_params
 		@url = '/employer/:id'
 		mail(to: @employer.email, subject: 'test welcome email')
 	end
