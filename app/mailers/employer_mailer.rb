70fe805f4b7a0345462836c1a7b984f6268cfd01
petedigipulse <peter@digitalpulse.co.nz>
2015-06-02 12:07:55 +1200

    Added mailer smtp settings & dynamic url link from email to login page

--- app/mailers/employer_mailer.rb
+++ app/mailers/employer_mailer.rb
@@ -1,8 +1,8 @@
 class EmployerMailer < ApplicationMailer
 	default from: 'peter@digitalpulse.co.nz'
-	def welcome_email(employer)
-		@employer = employer
-		@url = 
+	def welcome_email(employer_params)
+		@employer = employer_params(params[:id], params[:first_name])
+		@url = '/employer/:id'
 		mail(to: @employer.email, subject: 'test welcome email')
 	end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,16 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.example.com', 
+    :port           => 25, 
+    :domain         => 'www.digitalpulse.co.nz'
+    :authentcation  =>  :login, 
+    :user_name      => 'www', 
+    :password       => 'secret'
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
