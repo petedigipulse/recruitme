e75dd29adac144ebba3ea963c5b4da9443e26e8c
petedigipulse <peter@digitalpulse.co.nz>
2015-06-07 05:32:57 +1200

    set up angular files myapp and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
 
 Employer.create( first_name: 'Pete', last_name: 'Duncan', email: 'pete@venividivici.com', company: 'Veni Vidi Vici', phone: '2309493', password: '1234', image_file_name: './assets/images/venividivici.png')
 
